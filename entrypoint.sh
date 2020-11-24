a3aef82a26601541ac78f24f7ee15be21969dacf
Fernando <fernando.carvalho@paygo.com.br>
2020-11-24 10:01:32 -0300

    make aws-region required

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -x
 echo "set to stop executing after error"
 set -e 
 
-echo "Starting Jekyll build"
+echo "Start Jekyll build"
 echo "Set workspace permissions"
 chmod -R a+w /github/workspace
 
@@ -14,7 +14,7 @@ echo "Build the Jekyll site"
 jekyll build --trace
 echo "Completed Jekll build"
 
-echo "configure aws credentials"
+echo "Configure aws credentials"
 aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
@@ -22,15 +22,17 @@ ${AWS_REGION}
 text
 EOF
 
-echo "sync to aws s3"
+echo "Sync to aws s3"
 aws s3 sync ./_site/ \
             s3://${AWS_S3_BUCKET} \
             --delete \
             --profile s3-sync-action
 
-echo "invalidate cloudfront"
+echo "Invalidate cloudfront"
 aws cloudfront create-invalidation \
               --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION_ID} \
               --paths '/*'
+
+echo "Success Finished"              
 
 
